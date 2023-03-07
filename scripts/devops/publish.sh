f906066695147325d6b76ea7ebb8befcfb46e8ab
Andrew Kariuki <andrew.kariuki@wiocc.net>
2023-03-07 10:37:57 +0300

    chore: improve on structure

--- scripts/devops/publish.sh
+++ scripts/devops/publish.sh
@@ -8,7 +8,7 @@ if [ "$(echo "$CONTAINER_REGISTRY_TOKEN" | docker login ghcr.io -u andrewkariuki
 fi
 
 for VERSION in "$(date "+%Y.%-m.%-d")" "$GIT_TAGGED" "latest"; do
-    docker build -t ghcr.io/andrewkariuki/ospinsight-service:"$VERSION" .
+    docker build -t ghcr.io/andrewkariuki/eirene:"$VERSION" .
 done
 
-docker push ghcr.io/andrewkariuki/ospinsight-service --all-tags
+docker push ghcr.io/andrewkariuki/eirene --all-tags
